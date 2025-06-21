8ea33aa3665952395619fa88eb6bedd11826625e
BarakOren <shaharoren1234@gmail.com>
2025-06-21 23:30:45 +0300

    update

--- render-build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-# Update apt & install dependencies
-apt-get update
-apt-get install -y libnss3 libatk-bridge2.0-0 libxkbcommon-x11-0 libdrm2 libgbm1
-
-# Install Playwright browsers using node_modules/.bin
-npx playwright install chromium
